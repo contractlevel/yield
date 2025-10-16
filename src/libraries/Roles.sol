7dfc608137b3228d4c0451da6fd5e31b55f627a3
george-judge <george@judge.finance>
2025-10-15 19:24:25 -0600

    Updated Roles library with 2nd round of brainstorming/suggestions

--- src/libraries/Roles.sol
+++ src/libraries/Roles.sol
@@ -1,37 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-/// @notice Library for YieldCoin system roles
+/// @notice Library for YieldCoin custom roles
 library Roles {
     /// @dev example
-    /// bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    /// bytes32 public constant ROLE = keccak256("ROLE");
 
-    // @review George: Brain storming roles
+    /// @notice Function calls have been included as example of possible controls for each role.
 
-    /// @dev setInitialActiveStrategy - probably HAS to be the OWNER who deploys the contracts
+    /// @notice General admin role to grant/revoke roles. Contracts owner?
+    /// - setCCIPAdmin - (in token/Share.sol)
+    bytes32 public constant DEFAULT_ADMIN_ROLE = keccak256("DEFAULT_ADMIN_ROLE");
 
-    /// @notice Role for adding/remove strategys
-    /// - setStrategyAdapter
-    bytes32 public constant STRATEGY_MANAGER_ROLE = keccak256("STRATEGY_MANAGER_ROLE");
-
-    /// @notice Role for setting and withdrawing fees
-    /// - withdrawFees
-    /// - setFeeRate
-    bytes32 public constant FEE_MANAGER_ROLE = keccak256("FEE_MANAGER_ROLE");
-
-    /// @notice Role for setting integrating contract addresses - a "system manager" of sorts
-    /// - setUpkeepAddress
-    /// - setForwarder
-    /// - setParentPeer
-    /// - setStrategyRegistry
-    /// - setRebalancer
-    /// - setStrategyRegistry
+    /// @notice Setter of non-cross chain settings across contracts.
+    /// - setUpkeepAddress - (in modules/Rebalancer.sol)
+    /// - setForwarder --------------------^
+    /// - setParentPeer -------------------^
+    /// - setStrategyRegistry -------------^
+    /// - setStrategyRegistry - (in peers/YieldPeer.sol)
+    /// - setRebalancer - (in peers/ParentPeer.sol)
+    /// - setStrategyAdapter - (in modules/StrategyRegistry.sol)
     bytes32 public constant INTEGRATION_MANAGER_ROLE = keccak256("INTEGRATION_MANAGER_ROLE");
 
-    /// @notice Role for setting CCIP specific settings
-    /// - setAllowedChain
-    /// - setAllowedPeer
-    /// - setCCIPAdmin
-    /// - setCCIPGasLimit
-    bytes32 public constant CCIP_MANAGER_ROLE = keccak256("CCIP_MANAGER_ROLE");
+    /// @notice Role for Chainlink CCIP and other (possible future) cross chain settings.
+    /// - setAllowedChain - (in peers/YieldPeer.sol)
+    /// - setAllowedPeer - (in peers/YieldPeer.sol)
+    /// - setCCIPGasLimit - (in peers/YieldPeer.sol)
+    bytes32 public constant CROSS_CHAIN_MANAGER_ROLE = keccak256("CROSS_CHAIN_MANAGER_ROLE");
+
+    /// @notice Role for pausing and unpausing the system in case of emergency.
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant UNPAUSER_ROLE = keccak256("UNPAUSER_ROLE ");
+
+    /// @notice Role for withdrawing fees from each Peer.
+    /// - withdrawFees - (in modules/YieldFees.sol)
+    bytes32 public constant FEE_WITHDRAWER_ROLE = keccak256("FEE_WITHDRAWER_ROLE");
+
+    /// @notice Role for setting and controller the system fee rate.
+    /// - setFeeRate - (in modules/YieldFees.sol)
+    bytes32 public constant FEE_RATE_CONTROLLER_ROLE = keccak256("FEE_RATE_CONTROLLER_ROLE");
 }
