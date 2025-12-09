9264a7f92066c30e494a9e853ca4e03124b3645b
george-judge <george@judge.finance>
2025-12-09 14:12:40 -0700

    Rebalancer unit tests for ccip txs, minor spdx add fix

--- test/unit/rebalancer/OnReport.t.sol
+++ test/unit/rebalancer/OnReport.t.sol
@@ -100,4 +100,77 @@ contract OnReportTest is BaseTest {
         assertTrue(invalidProtocolIdEventFound);
         assertEq(emittedProtocolId, newStrategy.protocolId);
     }
+
+    function test_yield_rebalancer_onReport_rebalanceParentToChild() public {
+        /// @dev Arrange: Strategy on parent, deposit on parent to have TVL
+        _selectFork(baseFork);
+
+        deal(address(baseUsdc), depositor, DEPOSIT_AMOUNT);
+        _changePrank(depositor);
+        baseUsdc.approve(address(baseParentPeer), DEPOSIT_AMOUNT);
+        baseParentPeer.deposit(DEPOSIT_AMOUNT);
+
+        // Store TVL to verify in event
+        uint256 totalValue = baseParentPeer.getTotalValue();
+
+        // Create workflow metadata and report
+        bytes10 workflowName = _createWorkflowName(workflowNameRaw);
+        bytes memory metadata = _createWorkflowMetadata(workflowId, workflowName, workflowOwner);
+        IYieldPeer.Strategy memory newStrategy =
+            IYieldPeer.Strategy({chainSelector: optChainSelector, protocolId: keccak256(abi.encodePacked("aave-v3"))});
+        bytes memory encodedReport = _createWorkflowReport(newStrategy.chainSelector, newStrategy.protocolId);
+
+        /// @dev Act
+        _changePrank(keystoneForwarder);
+        vm.recordLogs();
+        baseRebalancer.onReport(metadata, encodedReport);
+
+        /// @dev Assert: Check for CCIPMessageSent event with correct tx type and value
+        /// @dev Tx type should be RebalanceNewStrategy as strategy is moving
+        /// @dev from Parent > Child and handled by ParentPeer::_rebalanceParentToChild
+        bytes32 ccipMessageSentEvent = keccak256("CCIPMessageSent(bytes32,uint8,uint256)");
+        bool ccipMessageSentEventFound = false;
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+        for (uint256 i = 0; i < logs.length; i++) {
+            if (logs[i].topics[0] == ccipMessageSentEvent) {
+                ccipMessageSentEventFound = true;
+                assertEq(uint8(uint256(logs[i].topics[2])), uint8(IYieldPeer.CcipTxType.RebalanceNewStrategy));
+                assertEq(uint256(logs[i].topics[3]), totalValue);
+            }
+        }
+        assertTrue(ccipMessageSentEventFound, "CCIPMessageSent log not found");
+    }
+
+    function test_yield_rebalancer_onReport_rebalanceChildToOther() public {
+        /// @dev Arrange: Change Strategy to be on a Child (eth child)
+        _setStrategy(ethChainSelector, keccak256(abi.encodePacked("aave-v3")), SET_CROSS_CHAIN);
+
+        // Create workflow metadata and report
+        bytes10 workflowName = _createWorkflowName(workflowNameRaw);
+        bytes memory metadata = _createWorkflowMetadata(workflowId, workflowName, workflowOwner);
+        IYieldPeer.Strategy memory newStrategy =
+            IYieldPeer.Strategy({chainSelector: optChainSelector, protocolId: keccak256(abi.encodePacked("aave-v3"))});
+        bytes memory encodedReport = _createWorkflowReport(newStrategy.chainSelector, newStrategy.protocolId);
+
+        /// @dev Act
+        _selectFork(baseFork);
+        _changePrank(keystoneForwarder);
+        vm.recordLogs();
+        baseRebalancer.onReport(metadata, encodedReport);
+
+        /// @dev Assert: Check for CCIPMessageSent event with correct tx type
+        /// @dev Tx type should be RebalanceOldStrategy as strategy is moving
+        /// @dev from Child > Other (Child) and handled by ParentPeer::_rebalanceChildToOther
+        bytes32 ccipMessageSentEvent = keccak256("CCIPMessageSent(bytes32,uint8,uint256)");
+        bool ccipMessageSentEventFound = false;
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+        for (uint256 i = 0; i < logs.length; i++) {
+            if (logs[i].topics[0] == ccipMessageSentEvent) {
+                ccipMessageSentEventFound = true;
+                assertEq(uint8(uint256(logs[i].topics[2])), uint8(IYieldPeer.CcipTxType.RebalanceOldStrategy));
+                assertEq(uint256(logs[i].topics[3]), 0);
+            }
+        }
+        assertTrue(ccipMessageSentEventFound, "CCIPMessageSent log not found");
+    }
 }
