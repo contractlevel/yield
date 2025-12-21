f2d32fd2548e4cff168ccca6509ba03b66c0d6a4
contract-level <contractlevel@proton.me>
2025-12-21 11:21:40 +0000

    add PoolAddressProvider and Pool to bindings.go and interfaces.go

--- cre/cre-rebalance/apy-impact-calculator/internal/aavev3/bindings.go
+++ cre/cre-rebalance/apy-impact-calculator/internal/aavev3/bindings.go
@@ -5,10 +5,30 @@ import (
 
 	"cre-rebalance/contracts/evm/src/generated/aave_protocol_data_provider"
 	"cre-rebalance/contracts/evm/src/generated/default_reserve_interest_rate_strategy"
+	// @review these need to be added to contracts/evm/src/generated/
+	// "cre-rebalance/contracts/evm/src/generated/pool"
+	// "cre-rebalance/contracts/evm/src/generated/pool_addresses_provider"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/smartcontractkit/cre-sdk-go/capabilities/blockchain/evm"
 )
+
+func NewPoolAddressesProviderBinding(client *evm.Client, addr string) (PoolAddressesProviderInterface, error) {
+	if !common.IsHexAddress(addr) {
+		return nil, fmt.Errorf("invalid PoolAddressesProvider address: %s", addr)
+	}
+	providerAddr := common.HexToAddress(addr)
+
+	return pool_addresses_provider.NewPoolAddressesProvider(
+		client,
+		providerAddr,
+		nil,
+	)
+}
+
+func NewPoolBinding(client *evm.Client, poolAddr common.Address) (PoolInterface, error) {
+	return pool.NewPool(client, poolAddr, nil)
+}
 
 // NewAaveProtocolDataProviderBinding constructs the Aave Protocol Data Provider binding.
 // It validates the address and returns an interface for testability.
@@ -25,15 +45,15 @@ func NewAaveProtocolDataProviderBinding(client *evm.Client, addr string) (AavePr
 	)
 }
 
-// NewDefaultReserveInterestRateStrategyBinding constructs the Interest Rate Strategy binding.
+// NewDefaultReserveInterestRateStrategyV2Binding constructs the Interest Rate Strategy binding.
 // It validates the address and returns an interface for testability.
-func NewDefaultReserveInterestRateStrategyBinding(client *evm.Client, addr string) (DefaultReserveInterestRateStrategyInterface, error) {
+func NewDefaultReserveInterestRateStrategyV2Binding(client *evm.Client, addr string) (DefaultReserveInterestRateStrategyV2Interface, error) {
 	if !common.IsHexAddress(addr) {
-		return nil, fmt.Errorf("invalid DefaultReserveInterestRateStrategy address: %s", addr)
+		return nil, fmt.Errorf("invalid DefaultReserveInterestRateStrategyV2 address: %s", addr)
 	}
 	strategyAddr := common.HexToAddress(addr)
 
-	return default_reserve_interest_rate_strategy.NewDefaultReserveInterestRateStrategy(
+	return default_reserve_interest_rate_strategy.NewDefaultReserveInterestRateStrategyV2(
 		client,
 		strategyAddr,
 		nil, // No filter options needed for reads
--- cre/cre-rebalance/apy-impact-calculator/internal/aavev3/interfaces.go
+++ cre/cre-rebalance/apy-impact-calculator/internal/aavev3/interfaces.go
@@ -10,34 +10,42 @@ import (
 	"github.com/smartcontractkit/cre-sdk-go/cre"
 )
 
+type PoolAddressesProviderInterface interface {
+	GetPool(runtime cre.RunTime, blockNumber *big.Int) cre.Promise[common.Address]
+}
+
+type PoolInterface interface {
+	GetReserveData(runtime cre.RunTime, input pool.GetReserveDataInput, blockNumber *big.Int) cre.Promise[aave_protocol_data_provider.GetReserveDataOutput]
+	GetConfiguration(runtime cre.RunTime, input pool.GetConfigurationInput, blockNumber *big.Int) cre.Promise[aave_protocol_data_provider.GetConfigurationOutput]
+}
+
 // AaveProtocolDataProviderInterface abstracts the Aave Protocol Data Provider contract
 // @input Asset Address
 type AaveProtocolDataProviderInterface interface {
+	// @review we probably dont need this
 	GetReserveData(
 		runtime cre.Runtime,
 		input aave_protocol_data_provider.GetReserveDataInput,
 		blockNumber *big.Int,
 	) cre.Promise[aave_protocol_data_provider.GetReserveDataOutput]
 
-	GetReserveConfigurationData(
-		runtime cre.Runtime,
-		input aave_protocol_data_provider.GetReserveConfigurationDataInput,
-		blockNumber *big.Int,
-	) cre.Promise[aave_protocol_data_provider.GetReserveConfigurationDataOutput]
+	GetTotalDebt(runtime cre.RunTime, input aave_protocol_data_provider.GetTotalDebtInput, blockNumber *big.Int) cre.Promise[*big.Int]
 
-	GetInterestRateStrategyAddress(
-		runtime cre.Runtime,
-		input aave_protocol_data_provider.GetInterestRateStrategyAddressInput,
-		blockNumber *big.Int,
-	) cre.Promise[common.Address]
 }
 
-// DefaultReserveInterestRateStrategyInterface abstracts the Interest Rate Strategy contract
+// DefaultReserveInterestRateStrategyV2Interface abstracts the Interest Rate Strategy contract
 // @input Asset Address
-type DefaultReserveInterestRateStrategyInterface interface {
+type DefaultReserveInterestRateStrategyV2Interface interface {
+	// @review we probably dont need this
 	GetInterestRateData(
 		runtime cre.Runtime,
 		input default_reserve_interest_rate_strategy.GetInterestRateDataInput,
 		blockNumber *big.Int,
 	) cre.Promise[default_reserve_interest_rate_strategy.IDefaultInterestRateStrategyV2InterestRateDataRay]
+
+	CalculateInterestRates(
+		runtime cre.Runtime,
+		input default_reserve_interest_rate_strategy.CalculateInterestRatesInput,
+		blockNumber *big.Int,
+	) cre.Promise[default_reserve_interest_rate_strategy.CalculateInterestRatesOutput]
 }
