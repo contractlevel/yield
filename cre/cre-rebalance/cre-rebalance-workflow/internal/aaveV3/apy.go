b02d2f493144c976eff20fec61fd6b8ccf6a340d
judgestef <stefanos@judge.finance>
2026-01-09 14:27:18 +0200

    Refactor GetAPY to use sequential .Await() pattern instead of cre.ThenPromise chaining

--- cre/cre-rebalance/cre-rebalance-workflow/internal/aaveV3/apy.go
+++ cre/cre-rebalance/cre-rebalance-workflow/internal/aaveV3/apy.go
@@ -55,40 +55,42 @@ func GetAPY(config *helper.Config, runtime cre.Runtime, liquidityAdded *big.Int,
 		return 0, fmt.Errorf("failed to create PoolAddressesProvider binding for chain %s: %w", evmCfg.ChainName, err)
 	}
 
-	// Step 3: Get ProtocolDataProvider binding (helper reduces nesting)
+	// Step 3: Get ProtocolDataProvider binding
 	protocolDataProviderPromise := getProtocolDataProviderBinding(runtime, evmClient, poolAddressesProvider, evmCfg.ChainName)
+	protocolDataProvider, err := protocolDataProviderPromise.Await()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get ProtocolDataProvider for chain %s: %w", evmCfg.ChainName, err)
+	}
 
-	// Step 4: Get Strategy binding and pass ProtocolDataProvider through for params
-	apyPromise := cre.ThenPromise(protocolDataProviderPromise, func(protocolDataProvider AaveProtocolDataProviderInterface) cre.Promise[float64] {
-		// Get USDC address
-		usdcAddress := common.HexToAddress(evmCfg.USDCAddress)
+	// Step 4: Get USDC address
+	usdcAddress := common.HexToAddress(evmCfg.USDCAddress)
 
-		// Get Strategy binding
-		strategyPromise := getStrategyBinding(runtime, evmClient, protocolDataProvider, usdcAddress, evmCfg.ChainName)
+	// Step 5: Get Strategy binding
+	strategyPromise := getStrategyBinding(runtime, evmClient, protocolDataProvider, usdcAddress, evmCfg.ChainName)
+	strategyV2, err := strategyPromise.Await()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get Strategy binding for chain %s: %w", evmCfg.ChainName, err)
+	}
 
-		// Step 5: Fetch params and calculate APY
-		return cre.ThenPromise(strategyPromise, func(strategyV2 DefaultReserveInterestRateStrategyV2Interface) cre.Promise[float64] {
-			// Step 6: Fetch CalculateInterestRatesParams
-			paramsPromise := FetchCalculateInterestRatesParams(
-				runtime,
-				protocolDataProvider,
-				usdcAddress,
-				liquidityAdded,
-			)
+	// Step 6: Fetch CalculateInterestRatesParams
+	paramsPromise := FetchCalculateInterestRatesParams(
+		runtime,
+		protocolDataProvider,
+		usdcAddress,
+		liquidityAdded,
+	)
+	params, err := paramsPromise.Await()
+	if err != nil {
+		return 0, fmt.Errorf("failed to fetch CalculateInterestRatesParams for chain %s: %w", evmCfg.ChainName, err)
+	}
 
-			// Step 7: Calculate APY using the strategy contract
-			return cre.ThenPromise(paramsPromise, func(params *CalculateInterestRatesParams) cre.Promise[float64] {
-				logger.Info("GetAPY: Got CalculateInterestRatesParams",
-					"chain", evmCfg.ChainName,
-					"totalDebt", params.TotalDebt.String(),
-					"virtualUnderlyingBalance", params.VirtualUnderlyingBalance.String())
+	logger.Info("GetAPY: Got CalculateInterestRatesParams",
+		"chain", evmCfg.ChainName,
+		"totalDebt", params.TotalDebt.String(),
+		"virtualUnderlyingBalance", params.VirtualUnderlyingBalance.String())
 
-				return CalculateAPYFromContract(runtime, strategyV2, params)
-			})
-		})
-	})
-
-	// Await for sychronousity for APY to be used in any workflow
+	// Step 7: Calculate APY using the strategy contract
+	apyPromise := CalculateAPYFromContract(runtime, strategyV2, params)
 	apy, err := apyPromise.Await()
 	if err != nil {
 		return 0, fmt.Errorf("failed to calculate APY for chain %s: %w", evmCfg.ChainName, err)
@@ -100,105 +102,3 @@ func GetAPY(config *helper.Config, runtime cre.Runtime, liquidityAdded *big.Int,
 
 	return apy, nil
 }
-
-// func CalculateAPY(config *helper.Config, liquidityAdded *big.Int, reserve common.Address) (*big.Int, error) {
-// 	interestRatesParams, err := getCalculateInterestRatesParams(config, liquidityAdded, reserve)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to get calculate interest rates params: %w", err)
-// 	}
-
-// 	// instantiate DefaultReserveInterestRateStrategyV2 - this will be optimized later
-// 	defaultReserveInterestRateStrategyV2, err := NewDefaultReserveInterestRateStrategyV2Binding(evmClient, config.DefaultReserveInterestRateStrategyV2Addr)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to create defaultReserveInterestRateStrategyV2 binding: %w", err)
-// 	}
-
-// 	// @review i dont think the second param is an error... is it? something else gets returned by calculateInterestRates besides the liquidity rate
-// 	liquidityRate, err := defaultReserveInterestRateStrategyV2.CalculateInterestRates(evmClient, interestRatesParams, nil)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to calculate interest rates: %w", err)
-// 	}
-
-// 	apr := liquidityRate / RAY
-
-// 	// @review this needs review
-// 	apy := (1 + apr / SECONDS_PER_YEAR) ^ SECONDS_PER_YEAR - 1
-
-// 	return apy, nil
-// }
-
-// func getCalculateInterestRatesParams(config *helper.Config, liquidityAdded *big.Int, reserve common.Address) (CalculateInterestRatesParams, error) {
-
-// 	// this will be optimized later
-// 	evmClient := &evm.Client{
-// 		ChainSelector: config.Evms[0].ChainSelector, // @review which evm is being used
-// 	}
-
-// 	// instantiate pool and protocolDataProvider - this will be optimized later
-// 	poolAddressesProvider, err := NewPoolAddressesProviderBinding(evmClient, config.PoolAddressesProviderAddr)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to create poolAddressesProvider binding: %w", err)
-// 	}
-// 	poolAddress, err := poolAddressesProvider.GetPool(evmClient, nil)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to get pool address: %w", err)
-// 	}
-// 	pool, err := NewPoolBinding(evmClient, poolAddress)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to create pool binding: %w", err)
-// 	}
-// 	// @review does the AaveProtocolDataProvider address change like pool or is it immutable like PoolAddressesProvider?
-// 	protocolDataProvider, err := NewAaveProtocolDataProviderBinding(evmClient, config.ProtocolDataProviderAddr)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to create protocolDataProvider binding: %w", err)
-// 	}
-
-// 	// create CalculateInterestRatesParams
-// 	reserveData, err := pool.GetReserveData(evmClient, pool.GetReserveDataInput{Asset: reserve}, nil)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to get reserve data: %w", err)
-// 	}
-// 	unbacked := reserveData.Unbacked
-// 	liquidityTaken := big.NewInt(0)
-// 	totalDebt, err := protocolDataProvider.GetTotalDebt(evmClient, protocolDataProvider.GetTotalDebtInput{Asset: reserve}, nil)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to get total debt: %w", err)
-// 	}
-// 	reserveConfigurationData, err := protocolDataProvider.GetReserveConfigurationData(evmClient, protocolDataProvider.GetReserveConfigurationDataInput{Asset: reserve}, nil)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("failed to get reserve configuration data: %w", err)
-// 	}
-// 	reserveFactor := reserveConfigurationData.ReserveFactor
-// 	usingVirtualBalance := false
-// 	virtualUnderlyingBalance := big.NewInt(0)
-
-// 	return CalculateInterestRatesParams{
-// 		Unbacked: unbacked,
-// 		LiquidityAdded: liquidityAdded,
-// 		LiquidityTaken: liquidityTaken,
-// 		TotalDebt: totalDebt,
-// 		ReserveFactor: reserveFactor,
-// 		Reserve: reserve,
-// 		UsingVirtualBalance: usingVirtualBalance,
-// 		VirtualUnderlyingBalance: virtualUnderlyingBalance,
-// 	}, nil
-
-// }
-
-// unbacked = pool.getReserveData(asset).unbacked
-// liquidityAdded = 0 for current APY, amount we are depositing for new APY
-// liquidityTaken = 0 (we arent simulating withdraws/borrows)
-// totalDebt = protocolDataProvider.getReserveData(asset) totalVariableDebt + totalStableDebt (totalStableDebt is deprecated)
-// reserveFactor = protocolDataProvider.getReserveConfigurationData(asset).reserveFactor (thing we already got that gets taken away)
-// reserve = stablecoin address (USDC)
-// usingVirtualBalance = deprecated
-// virtualUnderlyingBalance = deprecated
-
-// Unbacked                 *big.Int
-// LiquidityAdded           *big.Int
-// LiquidityTaken           *big.Int
-// TotalDebt                *big.Int
-// ReserveFactor            *big.Int
-// Reserve                  common.Address
-// UsingVirtualBalance      bool
-// VirtualUnderlyingBalance *big.Int
