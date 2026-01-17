de13779327803de87aceddb1ee0b9ab8ed7b9f64
contract-level <contractlevel@proton.me>
2026-01-17 20:54:32 +0000

    add comment on block num for simulate

--- cre/rebalance/workflow/internal/constants/constants.go
+++ cre/rebalance/workflow/internal/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	LatestFinalizedBlock = int64(-3)
+	LatestFinalizedBlock = int64(-3) // @review use -2 for forked staging simulation
 	WAD = 1e18
 	SecondsPerYear = 31536000
 )
